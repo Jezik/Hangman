fda186ed34e6f75a83b9c06355883ab2b743b505
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-20 16:34:44 +0300

    If-else implementation for guessing the world 'python'

--- src/Hangman.py
+++ src/Hangman.py
@@ -1,2 +1,8 @@
 print("H A N G M A N")
-print("The game will be available soon.")
+
+rigth_answer = "python"
+word = input("Guess the world: ")
+if word == rigth_answer:
+    print("You survived!")
+else:
+    print("You are hanged!")
