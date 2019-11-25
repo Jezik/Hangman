30165b2d0dacb9275e316789cecdfb134d594c20
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-25 17:19:31 +0300

    Check enter. Check that the letter was entered. Refactoring

--- src/Hangman.py
+++ src/Hangman.py
@@ -1,25 +1,28 @@
 from random import choice
 
 # Checks input from a player
-def check_letter(letter, word, show_word):
-    if letter in word:
-        position_lst = list()
-        index = 0
-        while index < len(word):
-            index = word.find(letter, index)
-            if index == -1:
-                break
-            position_lst.append(index)
-            index += 1
-        for pos in position_lst:
-            if show_word[pos] == letter:
-                print("No improvements")
-                return False
-            show_word[pos] = letter 
-        return True
+def check_letter(letter, word, show_word, attempts):
+    if letter not in attempts:
+        if letter in word:
+            attempts.append(letter)
+            position_lst = list()
+            index = 0
+            while index < len(word):
+                index = word.find(letter, index)
+                if index == -1:
+                    break
+                position_lst.append(index)
+                index += 1
+            for pos in position_lst:
+                show_word[pos] = letter 
+            return True
+        else:
+            print("No such letter in the word")
+            attempts.append(letter)
+            return False
     else:
-        print("No such letter in the word")
-        return False
+        print("You already typed this letter")        
+        return True
 
 
 # Start of a program
@@ -28,6 +31,7 @@ print("H A N G M A N")
 words_list = ["python", "java", "kotlin", "javascript"]
 rigth_answer = choice(words_list)
 answer_to_show = ["-"] * len(rigth_answer)
+attempts_list = list()
 
 i = 0
 while i < 8:
@@ -37,9 +41,15 @@ while i < 8:
         print("You guessed the word!\nYou survived!")
         break
     letter = input("Input a letter: ")
-    if check_letter(letter, rigth_answer, answer_to_show):
-        continue
+    if len(letter) == 1:
+        if letter.islower():
+            if check_letter(letter, rigth_answer, answer_to_show, attempts_list):
+                continue
+            else:
+                i += 1
+                if i == 8:
+                    print("You are hanged!")
+        else:
+            print("It is not an ASCII lowercase letter")            
     else:
-        i += 1
-        if i == 8:
-            print("You are hanged!")
+        print("You should print a single letter")
