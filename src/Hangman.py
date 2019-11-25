6ecffe0f5e6fa54e885632af143c2b238d9cefa2
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-25 17:57:00 +0300

    Created main game loop. Ask player to play again or quit

--- src/Hangman.py
+++ src/Hangman.py
@@ -27,29 +27,35 @@ def check_letter(letter, word, show_word, attempts):
 
 # Start of a program
 print("H A N G M A N")
-
 words_list = ["python", "java", "kotlin", "javascript"]
-rigth_answer = choice(words_list)
-answer_to_show = ["-"] * len(rigth_answer)
-attempts_list = list()
 
-i = 0
-while i < 8:
-    print()
-    print("".join(answer_to_show))
-    if "-" not in answer_to_show:
-        print("You guessed the word!\nYou survived!")
-        break
-    letter = input("Input a letter: ")
-    if len(letter) == 1:
-        if letter.islower():
-            if check_letter(letter, rigth_answer, answer_to_show, attempts_list):
-                continue
+flag = True
+while flag:    
+    command = input("Type 'play' to play the game, 'exit' to quit: ")
+    if command == "play":
+        rigth_answer = choice(words_list)
+        answer_to_show = ["-"] * len(rigth_answer)
+        attempts_list = list()
+
+        i = 0
+        while i < 8:
+            print()
+            print("".join(answer_to_show))
+            if "-" not in answer_to_show:
+                print("You guessed the word!\nYou survived!\n")
+                break
+            letter = input("Input a letter: ")
+            if len(letter) == 1:
+                if letter.islower():
+                    if check_letter(letter, rigth_answer, answer_to_show, attempts_list):
+                        continue
+                    else:
+                        i += 1
+                        if i == 8:
+                            print("You are hanged!\n")
+                else:
+                    print("It is not an ASCII lowercase letter")            
             else:
-                i += 1
-                if i == 8:
-                    print("You are hanged!")
-        else:
-            print("It is not an ASCII lowercase letter")            
-    else:
-        print("You should print a single letter")
+                print("You should print a single letter")
+    elif command == "exit":
+        flag = False
